c839e064bbca2a8c9ba45410780ffa9ca82eb9b8
MSXgroup <msxgroup369@gmail.com>
2025-07-08 15:48:48 +0300

    Create config.js

---
+++ js/firebase/config.js
@@ -0,0 +1,12 @@
+// إعدادات Firebase الخاصة بك
+const firebaseConfig = {
+  apiKey: "ضع-API-Key-هنا",
+  authDomain: "msx-system.firebaseapp.com",
+  projectId: "msx-system",
+  storageBucket: "msx-system.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef123456"
+};
+
+// تهيئة Firebase
+firebase.initializeApp(firebaseConfig);
